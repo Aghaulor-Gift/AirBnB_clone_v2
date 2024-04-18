321d3dcf0b84fde339ec104b07cd0fef3e02bb90
Gift <aghaulor.gift@gmail.com>
2024-04-17 17:33:54 -0700

    Update create method

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,26 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        args = arg.split()
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        class_name = args.pop(0)
+        if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+
+        params = {}
+        for arg in args:
+            key, value = arg.split("=")
+            value = value.replace('"', "").replace("_", " ")
+            params[key] = float(value) if "." in value else int(value)\
+                if value.isdigit() else value
+
+        obj = self.__classes[class_name](**params)
+        print(obj.id)
+        models.storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -319,6 +329,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
