75f9748bdc6aa036f296a0757c67504fadbefd39
Gift <aghaulor.gift@gmail.com>
2024-05-05 14:07:48 -0700

    Fix the shebang

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,6 +6,7 @@ web_static folder of the AirBnB Clone repo, using the function do_pack
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """Generate a .tgz archive from the contents of the web_static folder."""
 
