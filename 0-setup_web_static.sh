7d9eabbd50f32658f6de18dcabcbbc99599a2eb3
Gift <aghaulor.gift@gmail.com>
2024-05-05 13:10:15 -0700

    Set up webserver

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Install Nginx if not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+  sudo apt-get update
+  sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html><head></head><body> Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i "/^\s*server_name\s/ a \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" "$config_file"
+sudo service nginx restart
