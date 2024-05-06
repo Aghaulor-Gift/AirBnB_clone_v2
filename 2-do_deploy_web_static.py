581cef4acc699383c396eb28acb3c22787f0823e
Gift <aghaulor.gift@gmail.com>
2024-05-06 15:58:21 -0700

    Update deploy function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,34 +5,39 @@ from fabric.api import env, put, run
 import os
 
 
+env.user = 'ubuntu'
+env.key_filename = '/path/to/your/ssh/private/key.pem'
 env.hosts = ['52.91.134.237', '54.158.211.199']
 
 
 def do_deploy(archive_path):
     """Distribute an archive to web servers and deploy it."""
-    
+
     # Check if the archive exists
     if not os.path.exists(archive_path):
         return False
 
     # Extract the archive filename without extension
-    filenam.ext = os.path.basename(archive_path)
-    filename = os.path.splitext(filename)[0]
+    filename_ext = os.path.basename(archive_path)
+    filename = os.path.splitext(filename_ext)[0]
 
-    # Upload the archive to /tmp/ directory on the servers
-    put(archive_path, '/tmp/')
-    
-    # Uncompress the archive to /data/web_static/releases/<archive filename without extension> on the servers
     run('mkdir -p /data/web_static/releases/{}/'.format(filename))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename.ext, filename))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+        filename_ext, filename))
 
     # Delete the archive from the servers
-    run('rm /tmp/{}'.format(filename.ext))
-    
+    run('rm /tmp/{}'.format(filename_ext))
+
+    # Move contents to the proper location and remove unnecessary folder
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases
+        /{}/'.format(filename, filename))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(filename))
+
     # Delete the symbolic link /data/web_static/current from the servers
     run('rm -rf /data/web_static/current')
-    
-    # Create a new symbolic link /data/web_static/current linked to the new version
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename))
+
+    # New symbolic link /data/web_static/current linked to the new version
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(
+        filename))
 
     return True
