54e3e75c26d18a1c81a3d2bc9fb37b19579e15cb
Gift <aghaulor.gift@gmail.com>
2024-05-17 10:06:10 -0700

    Add route decorator with text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application
+listening on 0.0.0.0, port 5000 and displays 'Hello HBNB!' on the homepage.
+/hbnb: display “HBNB”
+"""
+
+from flask import Flask
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root URL. Returns 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the root URL. Returns "HBNB".
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route handler for the /c/<text> URL.
+    Returns 'C ' followed by the value of the text variable,
+    with underscores replaced by spaces.
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
